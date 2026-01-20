bcb17abd4328a796c614c3103034e6bded2dff18
GeoffrayCM <muyombe@hotmail.fr>
2026-01-20 11:40:54 +0100

    3-print_reversed_list_integer.py

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-print_reversed_list_integer.py
+
+def print_reversed_list_integer(my_list=[]):
+        print_reversed_list_integer = _import_('3-print_reversed_list_integer').print_reversed_list_integer
+
+        my_list = [1,2,3]
+        print_reversed_list_integer(my_list)
