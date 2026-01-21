e11fb0033eac3a3a8b19ecafa73fa3c090516c73
GeoffrayCM <muyombe@hotmail.fr>
2026-01-21 11:53:42 +0100

    10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,11 +3,18 @@
 
 def divisble_by_2(my_list=[]):
         """finds all multiples of 2 in a list"""
+        
         multiples = []
+        
         for i in range(len(my_list)):
+            
             if my_list[i] % 2 == 0:
+                
                 multiples.append(true)
+        
         else:
+            
             multiples.append(false)
 
+        
         return (multiples)
