0209b1780d05169aed337ca3c43605a3d3b902ce
GeoffrayCM <muyombe@hotmail.fr>
2026-01-22 12:47:32 +0100

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             ret += 1
-    except IndexError:
-        break
+        except IndexError:
+            break
     print('')
-    return (ret)
+    return ret
 
