fac067db6e00c15e2bbff92192544491f6cd4cbc
GeoffrayCM <muyombe@hotmail.fr>
2026-01-28 12:20:16 +0100

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Defines a square."""
 
 
 class Square:
+    """Represents a square."""
 
     def _init_(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: size of the square
+        """
         self._size = size
