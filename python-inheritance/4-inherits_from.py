da0d92cae93fa2c5a9238843a337e2fea9b840d4
GeoffrayCM <muyombe@hotmail.fr>
2026-02-05 11:25:55 +0100

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class)
+"""Function that checks if an object is an instance of a subclass of a_class"""
+
+
+def inherits_from(obj, a_class):
+    # type(obj) is not a_class ensures obj is not exactly a_class
+    # isinstance(obj, a_class) checks if obj is an instance or inherited
+    return isinstance(obj, a_class) and type(obj) is not a_class
